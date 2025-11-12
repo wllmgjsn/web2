b277d2a25c18a9a6e9e5a0c392a34b26a646a65e
wllmgjsn <william.gijsen@student.vinci.be>
2025-11-12 22:45:02 +0100

    new:ex2.6b

--- exercises/2.6b/src/components/ColorPicker.tsx
+++ exercises/2.6b/src/components/ColorPicker.tsx
@@ -1,27 +1,24 @@
-import { useState } from "react"
-import './ColorPicker.css'
+import { useState } from "react";
+import "./ColorPicker.css";
 
-
-interface ColorPickerProps{
-
-}
+interface ColorPickerProps {}
 
 const ColorPickerBox = () => {
-    
-    const [color, changeColor] = useState('red');
-    
-    const handleColorChange = () => {
-        if(color === 'red'){changeColor('gree')}
-    }
+  const [colorIndex, incrementColorIndex] = useState(0);
+  const colors = ["red", "green", "yellow", "violet"];
 
+  const handleClick = () => {
+    incrementColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+  };
 
-    return(
-        <div className="colorBox">
-            <button onClick={handleColorChange}>
-
-            </button>
-        </div>
-    )
-}
+  return (
+    <div className="colorBox" style={{ backgroundColor: colors[colorIndex] }}>
+      <button onClick={handleClick}>
+        Next color is : {colors[(colorIndex + 1) % 4]}
+      </button>
+      <p>Color is : {colors[colorIndex]}</p>
+    </div>
+  );
+};
 
 export default ColorPickerBox;
