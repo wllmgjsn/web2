dde673cc83cc121f685b35a2b521eec95322d09f
wllmgjsn <william.gijsen@student.vinci.be>
2025-11-15 17:13:27 +0100

    new:ex2.7

---
+++ exercises/2.7/src/assets/components/Movie/list.tsx
@@ -0,0 +1,19 @@
+import type { Movie } from "../../../types";
+import MovieItem from ".";
+import "./index.css";
+
+interface MovieListProps {
+  movies: Movie[];
+}
+
+const MovieList = ({ movies }: MovieListProps) => {
+  return (
+    <ul className="movie-list">
+      {movies.map((movie) => (
+        <MovieItem key={movie.title} movie={movie}></MovieItem>
+      ))}
+    </ul>
+  );
+};
+
+export default MovieList;
