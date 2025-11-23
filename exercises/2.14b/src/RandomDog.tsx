c8f00f6362222241c40e71fab9ca3d3188220876
wllmgjsn <william.gijsen@student.vinci.be>
2025-11-23 22:36:22 +0100

    new:ex2.14b-refactor

---
+++ exercises/2.14b/src/RandomDog.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+
+interface Dog {
+  message: string;
+  status: string;
+}
+
+const RandomDog = () => {
+  const [dog, setDog] = useState<Dog | undefined>(undefined);
+
+  const fetchDogImage = async () => {
+    const response = await fetch("https://dog.ceo/api/breeds/image/random");
+
+    if (!response.ok)
+      throw new Error(
+        `fetch error : ${response.status} : ${response.statusText}`
+      );
+
+    const dog = await response.json();
+
+    setDog({
+      message: dog.message ?? "No dog found",
+      status: dog.status ?? "Error",
+    });
+  };
+
+  useEffect(() => {
+    fetchDogImage();
+  }, []);
+
+  if (!dog) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <h3>Random dog</h3>
+      <img src={dog.message} alt="Random dog" style={{ maxHeight: 300 }} />
+    </div>
+  );
+};
+
+export default RandomDog;
